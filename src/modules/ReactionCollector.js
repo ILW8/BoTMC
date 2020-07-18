70e47582ceffa5eaa89e6dd3de0a4f70fc3aea00
daohe <daohengliu@gmail.com>
2020-07-18 16:07:37 +0100

    changed error handling to actually print the error when bot can't fetch the user, added skeleton for :SadChamp: for #misdrop (reaction manager)

--- src/modules/ReactionCollector.js
+++ src/modules/ReactionCollector.js
@@ -8,7 +8,7 @@ module.exports = class ReactionCollector {
      * @param logger
      */
     constructor(collectorConfig, logger) {
-        this.logger = logger.child({ moduleLabel: "ReactionCollector" });  // https://github.com/winstonjs/winston/pull/1598
+        this.logger = logger.child({moduleLabel: "ReactionCollector"});  // https://github.com/winstonjs/winston/pull/1598
         this.logger.defaultMeta["moduleLabel"] = "ReactionCollector"
         Object.assign(this, collectorConfig);
     }
@@ -20,37 +20,38 @@ module.exports = class ReactionCollector {
         if (!this[messageIDTrace]) return;
 
         let roles, mappedRole = undefined
-        try
-        {
-            roles = reaction.message.guild.members.resolve(user).roles
 
-            //split(":").slice(-1).pop() returns substring starting from the last colon in the string, or the whole
-            //string if the string doesn't have a colon in it. To handle the fact that unicode emojis don't have an id.
-            mappedRole = this[messageIDTrace][reaction.emoji.identifier.split(":").slice(-1).pop()];
+        reaction.message.guild.members.fetch(user)
+            .then(guildMember => {
+                roles = guildMember.roles
+                //split(":").slice(-1).pop() returns substring starting from the last colon in the string, or the whole
+                //string if the string doesn't have a colon in it. To handle the fact that unicode emojis don't have an id.
+                mappedRole = this[messageIDTrace][reaction.emoji.identifier.split(":").slice(-1).pop()];
 
-            //if reaction not listed, remove reaction
-            if (!mappedRole) {
-                reaction.remove();
-                return;
-            }
-            //an `!` is added to the end of roles that are add-only
-            let addOnly = mappedRole.endsWith(`!`);
-            let roleID = addOnly ? mappedRole.slice(0, -1) : mappedRole;
+                //if reaction not listed, remove reaction
+                if (!mappedRole) {
+                    reaction.remove();
+                    return;
+                }
+                //an `!` is added to the end of roles that are add-only
+                let addOnly = mappedRole.endsWith(`!`);
+                let roleID = addOnly ? mappedRole.slice(0, -1) : mappedRole;
 
-            //else add/remove role
-            if (add) {
-                roles.add(roleID).then(
-                    () => this.logger.info(`Added ${roleID} to ${user.username}#${user.discriminator}`)
-                );
-            } else if (!addOnly) {
-                roles.remove(roleID).then(
-                    () => this.logger.info(`Removed ${roleID} from ${user.username}#${user.discriminator}`)
-                );
-            }
-        }
-        catch (TypeError)
-        {
-            this.logger.warn(`Could not fetch ${user.username}#${user.discriminator} (${user.id}), skipping.`)
-        }
+                //else add/remove role
+                if (add) {
+                    roles.add(roleID).then(
+                        () => this.logger.info(`Added ${roleID} to ${user.username}#${user.discriminator}`)
+                    );
+                } else if (!addOnly) {
+                    roles.remove(roleID).then(
+                        () => this.logger.info(`Removed ${roleID} from ${user.username}#${user.discriminator}`)
+                    );
+                }
+            })
+            .catch(error => {
+                this.logger.warn(`Could not fetch ${user.username}#${user.discriminator} (${user.id}), skipping. ${error}`)
+            })
+
+
     }
 }
