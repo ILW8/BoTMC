3831bd70bf72f64c4d343c5ca9f980ddfc4fe2d6
daohe <daohengliu@gmail.com>
2020-07-18 03:53:46 +0100

    added channel name and username padding

--- src/modules/MessageCollector.js
+++ src/modules/MessageCollector.js
@@ -8,14 +8,20 @@ module.exports = class MessageCollector {
                     (info) => {
                         let logMsg = ""
                         logMsg += `${info.timestamp} `
-                        logMsg += `[#${info.message.channel.name}] `
-                        logMsg += `${info.message.author.username}#${info.message.author.discriminator} `
+                        let chnPadLength = `[#${info.message.channel.name}] `.length > 16 ? 36 : 20
+                        let namePadLength = `${info.message.author.username}#${info.message.author.discriminator}`.length > 16 ? 32 : 16
+
+                        logMsg += `[#${info.message.channel.name}] `.padStart(chnPadLength)
+                        logMsg += `${info.message.author.username}#${info.message.author.discriminator} `.padStart(namePadLength)
                         logMsg += `(${info.message.author.id}): `
-                        logMsg += info.message.content !== "" ? `"${info.message.content}", ` : `<no text>, `
-                        logMsg += `attachments: `
-                        let attachments = []
-                        info.message.attachments.forEach((value, key) => attachments.push(value.attachment))
-                        logMsg += attachments.join(", ")
+                        logMsg += info.message.content !== "" ? `"${info.message.content}"` : `<no text>`
+                        if (info.message.attachments.size > 0)
+                        {
+                            logMsg += `, attachments: `
+                            let attachments = []
+                            info.message.attachments.forEach((value, key) => attachments.push(value.attachment))
+                            logMsg += attachments.join(", ")
+                        }
                         return logMsg
                     }
                 )),
